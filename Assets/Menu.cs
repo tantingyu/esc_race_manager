f6842857a07011f9d22e697c6ea25116dd9fb23f
tantingyu <32908553+tantingyu@users.noreply.github.com>
2018-02-07 22:02:43 +0800

    basic navigation added

---
+++ Assets/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    int menuIndex = 0;
+    int settingsIndex = 1;
+
+    public void goToSettings()
+    {
+        SceneManager.LoadScene(settingsIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
