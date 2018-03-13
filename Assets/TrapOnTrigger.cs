939527c7d3d93307a0735d23e4cdeaa67639e5b3
GUZHIYAO\admin <guzhiyao1234@gmail.com>
2018-03-13 21:24:05 +0800

    New Updates on Traps and players interaction

---
+++ Assets/TrapOnTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapOnTrigger : MonoBehaviour
+{
+
+    private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
