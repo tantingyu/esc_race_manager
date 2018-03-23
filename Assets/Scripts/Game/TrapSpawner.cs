4ed340381ace13f538d82da30cb4241f05f32c61
binaray@popon <raychengcx@live.com>
2018-03-23 22:08:27 +0900

    bg changes- etc

--- Assets/Scripts/Game/TrapSpawner.cs
+++ Assets/Scripts/Game/TrapSpawner.cs
@@ -4,24 +4,60 @@ using UnityEngine;
 
 public class TrapSpawner : MonoBehaviour {
 
-    public float spawnTime = 1f;
-    private float spawnTimer;
-    public Transform trap;
-    private readonly float[] position = { 1.32f, 0.12F, -1.08f };
+    public int startLevel = 0;
+    public Level[] levels=new Level[3];
+
+    private int currentLevel;
+    private readonly float[] position = { 1.32f, 0.12f, -1.08f };
 
     // Use this for initialization
     void Start () {
-		
+        currentLevel = startLevel;
+		foreach (Level level in levels)
+        {
+            level.spawnTimer = level.spawnTime;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        spawnTimer -= Time.deltaTime;
-        if (spawnTimer < 0)
+        levels[currentLevel].levelDuration -= Time.deltaTime;
+        levels[currentLevel].spawnTimer -= Time.deltaTime;
+
+        //instantiates random trap defined from level every spawn time
+        if (levels[currentLevel].spawnTimer < 0)
         {
-            int randY = Random.Range(0, 3);
-            Instantiate(trap, new Vector2(transform.position.x, position[randY]), Quaternion.identity);
-            spawnTimer = spawnTime;
+            int randTrap = Random.Range(0, levels[currentLevel].trapsToSpawn.Length);
+            //int randY = Random.Range(0, 3);
+            
+            if (RandomBool())
+                Instantiate(levels[currentLevel].trapsToSpawn[randTrap], new Vector2(transform.position.x, position[0]), Quaternion.identity);
+            if (RandomBool())
+                Instantiate(levels[currentLevel].trapsToSpawn[randTrap], new Vector2(transform.position.x, position[1]), Quaternion.identity);
+            if (RandomBool())
+                Instantiate(levels[currentLevel].trapsToSpawn[randTrap], new Vector2(transform.position.x, position[2]), Quaternion.identity);
+            levels[currentLevel].spawnTimer = levels[currentLevel].spawnTime;
         }
+
+        //increase difficulty level when duration ends
+        if (levels[currentLevel].levelDuration < 0 && currentLevel < levels.Length-1)
+            currentLevel++;
+        
     }
+    
+    bool RandomBool()
+    {
+        return (Random.value > 0.5f);
+    }
+}
+
+[System.Serializable]
+public class Level
+{
+    public float levelDuration;
+    public float spawnTime = 2f;
+    public Transform[] trapsToSpawn = new Transform[1];
+
+    [HideInInspector]
+    public float spawnTimer;
 }
---
+++ Assets/Scripts/TrapLogic/TrapAssets/BaseTrap.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseTrap : MonoBehaviour {
+
+    public float damage = 20;
+    public int movePosition = 0;
+    public float speed = -7.4f;
+
+    private Rigidbody2D myRigidbody;
+    private Animator anim;
+    private Renderer rend;
+    private float delay = 0.4f;
+    private bool seen = false;
+    
+    // Use this for initialization
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+        anim = GetComponent<Animator>();
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rend.isVisible)
+            seen = true;
+        if (seen && !rend.isVisible)
+            Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //trigger animation state
+        Destroy(gameObject, anim.GetCurrentAnimatorStateInfo(0).length + delay);
+    }
+}
