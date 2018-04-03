0f17ff43280da7e8566e25ae08d170fd311cf68e
tantingyu <tingyu_tan@mymail.sutd.edu.sg>
2018-04-04 02:18:23 +0800

    quit button

--- Assets/NavigationBar.cs
+++ Assets/NavigationBar.cs
@@ -1,16 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class NavigationBar : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	public void Quit() {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
 	}
 }
