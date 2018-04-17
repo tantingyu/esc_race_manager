56b2c27db86c8f6b4a8bc733fa8f6a2a977ff392
tantingyu <tingyu_tan@mymail.sutd.edu.sg>
2018-04-18 01:58:02 +0800

    textmesh

--- Assets/Scripts/RacerLogic/SheildScript.cs
+++ Assets/Scripts/RacerLogic/SheildScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SheildScript : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class SheildScript : MonoBehaviour
     private float shieldDurability = 100;
     public AudioClip sheildSound;
 
+    public GameObject healthBar;
+    public Image foreground;
+
     //private RacerController playerController;
     //public GameObject player;
     // Use this for initialization
@@ -16,8 +20,11 @@ public class SheildScript : MonoBehaviour
     {
         //myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
         //myRigidbody = GetComponent<Rigidbody2D>();
-        SoundManager.instance.PlaySingle(sheildSound);
+        // SoundManager.instance.PlaySingle(sheildSound);
 
+        healthBar = Instantiate(healthBar, transform.position, Quaternion.identity);
+        foreground = healthBar.transform.GetChild(1).gameObject.GetComponent<Image>();
+        foreground.fillAmount = 1;
     }
 
     // Update is called once per frame
@@ -39,6 +46,7 @@ public class SheildScript : MonoBehaviour
             //Destroy(gameObject, anim.GetCurrentAnimatorStateInfo(0).length + delay);
             float damage = collision.gameObject.GetComponent<BaseTrap>().damage;
             shieldDurability -= damage;
+            foreground.fillAmount = shieldDurability / 100;
             Debug.Log("AT field Strength: "+shieldDurability);
         }
     }
